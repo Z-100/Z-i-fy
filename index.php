54130d1d42f5f882ae65e69b25ed2c19dc96c55b
N4choWasTaken <72740099+N4choWasTaken@users.noreply.github.com>
2021-04-26 11:32:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Z-I-fy</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav id = left>
+        <div id="top">
+            <img src="img/z-i-fy_trans.png" alt="logo" id="logo"">
+        </div>
+        <div id="nav-mid">
+            <ul>
+                <li><a>Home</a></li>  
+                <li><a>Search</a></li> 
+                <li><a>Libary</a></li> 
+            </ul>         
+        </div>
+        <div is="playlst">
+
+        </div>
+    </nav>
+    <div id="main">
+    
+    </div>
+    <div id="player">
+
+    </div>
+</body>
+</html>
