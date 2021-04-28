502b1dd373b4762a7fc023bfbfafc10e14b6225c
N4choWasTaken <72740099+N4choWasTaken@users.noreply.github.com>
2021-04-28 10:12:18 +0200

    Add files via upload

---
+++ library.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Z-I-fy</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+    <nav id = left>
+        <div id="top">
+            <img src="img/z-i-fy_trans.png" alt="logo" id="logo"">
+        </div>
+        <div id="nav-mid">
+            <ul>
+                <li><a href=index.php><img src="img/homeIcon.png" width="45vw" alt="">Home</a></li>  
+                <li><a href="searchfield.php"><img src="img/searchIcon.png" width="45vw" alt="">Search</a></li> 
+                <li><a href="library.php"><img src="img/libraryIcon.png" width="45vw" alt="">Library</a></li> 
+            </ul>         
+        </div> 
+        <div id="playlist">
+            <?php 
+            // on Work
+             $currenUser = 1;
+                require_once "config.php";
+                $sql = "SELECT name
+                        FROM playlists WHERE user_id = 1";
+
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<li>";
+                            echo "<a>" . $row['name'] . "</a>";
+                        echo "</li>";
+
+                    }
+                } else {
+
+                }
+            ?>
+        </div>
+    </nav>
+    <div id="main">
+        <div id="top">
+            <div id="userField">
+            <?php
+                require_once "config.php";
+                $sql = "SELECT *
+                        FROM users WHERE id = 1";
+
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                            echo "<p>" . $row['username'] . "</p>";
+                    }
+                } else {
+
+                }
+            ?>
+            </div>
+        </div>
+        <div id="main-items">
+            <?php
+
+            ?>
+
+
+        </div>
+
+    </div>
+    <div id="player">
+            
+    </div>
+</body>
+</html>
